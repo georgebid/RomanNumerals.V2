acd5764eeb013dfbfcb1f0b0c1ddff3635926aec
georgebid <91198628+georgebid@users.noreply.github.com>
2021-12-10 14:39:51 +0000

    Add project files.

---
+++ Convertor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanNumerals.V2
+{
+    public class Convertor
+    {
+        public static int CalculateValue(string numeral)
+        {
+            switch (numeral)
+            {
+                case "I": return 1;
+                case "V": return 5;
+                case "X": return 10;
+                case "L": return 50;
+                case "C": return 100;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RomanNumerals.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Convertor convertor = new Convertor();
+            Console.WriteLine("Please enter a roman numeral: ");
+            string usersNumeral = Console.ReadLine().ToUpper();
+            Console.WriteLine(Convertor.CalculateValue(usersNumeral));
+        }
+    }
+}
