6f54e2c7cde8cd4923cd38e6fffbb1454ea3ed6b
georgebid <91198628+georgebid@users.noreply.github.com>
2021-12-17 11:32:56 +0000

    Added some comments

--- Convertor.cs
+++ Convertor.cs
@@ -6,6 +6,7 @@ namespace RomanNumerals.V2
 {
     public class Convertor
     {
+        // instance of a convertor rather than static.
         public static int CalculateValue(char numeral)
         {
             int numeralValue;
@@ -25,10 +26,15 @@ namespace RomanNumerals.V2
         {
             char[] romanNum;
             romanNum = numerals.ToCharArray();
+            Validation(romanNum);
+            // validation to run before anything else happens
             int result = 0;
             // created an array of valid numerals so that TI wouldn't return a value.
             char[] validNumerals = { 'I', 'V', 'X', 'L', 'C' };
+            // regex or strings could be used instead of a array, remove letters which aren't valid - from a string.
+
             // for loop that creates a value to product a result for each numeral entered.
+            // could loop backwards - works better logically. The witness game & human resource machine. 
             for (int i = 0; i < romanNum.Length; i++)
             {
                 // checks if the array is valid
@@ -55,5 +61,23 @@ namespace RomanNumerals.V2
             }
             return result;
         }
+        // below doesnt work.
+        // The idea is that it would check for invalid numeral, create error score and if the error score is 0 > then the convert method doesnt run.
+        public static int Validation(char[] romanNum)
+        {
+           
+            if (romanNum.Length > 4)
+            {
+                if (romanNum[0] == romanNum[1] && romanNum[0] == romanNum[2] && romanNum[0] == romanNum[3])
+                {
+                    bool invalidNumeral = true;
+                   if (invalidNumeral == true)
+                     {
+
+                    } 
+                }
+            }
+            return 0;
+        } 
     }
 }
